4acc17caabad010f3869ef12a123b12ea91f8df8
abeidahmed <abeidahmed92@gmail.com>
2021-01-12 19:02:53 +0530

    Check for valid hash setting key

---
+++ lib/microverse_twitterati/valid_setting.rb
@@ -0,0 +1,25 @@
+require_relative 'errors'
+
+module MicroverseTwitterati
+  class ValidSetting
+    ACCEPTED_KEYS = %i[consumer_key consumer_secret access_token access_token_secret].freeze
+
+    def initialize(options)
+      raise NotAHash unless options.is_a?(Hash)
+
+      @options = options
+    end
+
+    def check_valid_keys
+      options.each_key do |key|
+        return false unless ACCEPTED_KEYS.include?(key)
+      end
+
+      true
+    end
+
+    private
+
+    attr_reader :options
+  end
+end
---
+++ spec/microverse_twitterati/valid_setting_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../spec_helper'
+require './lib/microverse_twitterati/valid_setting'
+
+RSpec.describe MicroverseTwitterati::ValidSetting do
+  let(:valid_hash) { { consumer_key: 'world', consumer_secret: 'world' } }
+  let(:invalid_hash) { { hello: 'world', consumer_key: 'world' } }
+
+  describe '#initialize' do
+    it 'should raise an error if the arg is not an hash' do
+      expect { MicroverseTwitterati::ValidSetting.new('helloworld') }.to raise_error(MicroverseTwitterati::NotAHash)
+    end
+  end
+
+  describe '#check_valid_keys' do
+    it 'should return true if the keys are valid' do
+      setting = MicroverseTwitterati::ValidSetting.new(valid_hash)
+
+      expect(setting.check_valid_keys).to be_truthy
+    end
+
+    it 'should return false if the keys are invalid' do
+      setting = MicroverseTwitterati::ValidSetting.new(invalid_hash)
+
+      expect(setting.check_valid_keys).to be_falsy
+    end
+  end
+end
