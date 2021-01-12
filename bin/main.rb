9105f6b8b8322bc26cfa12459871496681e43894
abeidahmed <abeidahmed92@gmail.com>
2021-01-12 11:27:34 +0530

    Initial test for testing out the ENV variables

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,13 @@
 #!/usr/bin/env ruby
 
 require 'dotenv/load'
+require 'twitter'
 
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_API_KEY']
+  config.consumer_secret     = ENV['TWITTER_SECRET_KEY']
+  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
+end
+
+client.update('Hello world')
