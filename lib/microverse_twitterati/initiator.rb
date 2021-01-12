eccf0f8dd5e444f3e97dc9bdc6df373b041c4a5b
abeidahmed <abeidahmed92@gmail.com>
2021-01-12 13:28:36 +0530

    Fix rubocop errors

--- lib/microverse_twitterati/initiator.rb
+++ lib/microverse_twitterati/initiator.rb
@@ -16,9 +16,9 @@ module MicroverseTwitterati
 
     def initialize_retweet
       Twitter::REST::Client.new do |config|
-        config.consumer_key        = ENV['TWITTER_API_KEY']
-        config.consumer_secret     = ENV['TWITTER_SECRET_KEY']
-        config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+        config.consumer_key = ENV['TWITTER_API_KEY']
+        config.consumer_secret = ENV['TWITTER_SECRET_KEY']
+        config.access_token = ENV['TWITTER_ACCESS_TOKEN']
         config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
       end
     end
