7da9e8831c11fdf61faeae7f54b1ee532949943f
abeidahmed <abeidahmed92@gmail.com>
2021-01-12 17:43:56 +0530

    Run bot class

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,13 @@
 #!/usr/bin/env ruby
+
+require 'dotenv/load'
+require_relative '../lib/microverse_twitterati/bot'
+
+TWITTER_API_SETTINGS = {
+  consumer_key: ENV['TWITTER_API_KEY'],
+  consumer_secret: ENV['TWITTER_SECRET_KEY'],
+  access_token: ENV['TWITTER_ACCESS_TOKEN'],
+  access_token_secret: ENV['TWITTER_TOKEN_SECRET']
+}.freeze
+
+MicroverseTwitterati::Bot.new(TWITTER_API_SETTINGS).run!
